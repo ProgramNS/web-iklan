d3363ec782e242cf015c90acf8e3395f7af00d6d
ProgramNS <blise01@hotmail.com>
2024-11-12 14:53:10 +0700

    Fitur FE Sudah Selesai

---
+++ Admin/dataIklan.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!----======== CSS ======== -->
+    <link rel="stylesheet" href="../css/dashboard.css">
+    <link rel="stylesheet" href="../css/tabel.css">
+
+    <!----===== Iconscout CSS ===== -->
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <title>Data Iklan</title>
+</head>
+
+<body>
+    <nav>
+        <div class="logo-name">
+            <div class="logo-image">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/MNCTV_logo.png/798px-MNCTV_logo.png?20180109104219" alt="">
+            </div>
+
+            <span class="logo_name">Vision Panel</span>
+        </div>
+
+        <div class="menu-items">
+            <ul class="nav-links">
+                <li><a href="dashboard.php">
+                        <i class="uil uil-estate"></i>
+                        <span class="link-name">Dahsboard</span>
+                    </a></li>
+                <li><a href="dataIklan.php">
+                        <i class="uil uil-files-landscapes"></i>
+                        <span class="link-name">Iklan</span>
+                    </a></li>
+                <li><a href="transaksi.php">
+                        <i class="uil uil-chart"></i>
+                        <span class="link-name">Transaksi</span>
+                    </a></li>
+                <li><a href="dataPelanggan.php">
+                        <i class="uil uil-user"></i>
+                        <span class="link-name">Pengguna</span>
+                    </a></li>
+            </ul>
+
+            <ul class="logout-mode">
+                <li><a href="#">
+                        <i class="uil uil-signout"></i>
+                        <span class="link-name">Logout</span>
+                    </a></li>
+
+                <li class="mode">
+                    <a href="#">
+                        <i class="uil uil-moon"></i>
+                        <span class="link-name">Dark Mode</span>
+                    </a>
+
+                    <div class="mode-toggle">
+                        <span class="switch"></span>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <section class="dashboard">
+        <div class="top">
+            <i class="uil uil-bars sidebar-toggle"></i>
+        </div>
+        <div class="table-container">
+            <br>
+            <br>
+        <div class="table-header">
+            <h2>Pengajuan Iklan</h2>
+            <div class="search-box">
+                <input type="text" placeholder="Cari data...">
+                <i class="fas fa-search"></i>
+            </div>
+        </div>
+        <div class="table-responsive">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nama Pengguna</th>
+                        <th>Email</th>
+                        <th>Biaya</th>
+                        <th>Status</th>
+                        <th>Tanggal Iklan</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Yoga</td>
+                        <td>yoga@gmail.com</td>
+                        <td>Rp.100000</td>
+                        <td>Pending</td>
+                        <td>12-12-2024</td>
+                        <td>
+                            <div class="action-btn">
+                                <span class="edit-btn"><i class="fas fa-edit"></i> Edit</span>
+                                <span class="delete-btn"><i class="fas fa-trash"></i> Hapus</span>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    </section>
+    <script>
+        const body = document.querySelector("body"),
+            modeToggle = body.querySelector(".mode-toggle");
+        sidebar = body.querySelector("nav");
+        sidebarToggle = body.querySelector(".sidebar-toggle");
+
+        let getMode = localStorage.getItem("mode");
+        if (getMode && getMode === "dark") {
+            body.classList.toggle("dark");
+        }
+
+        let getStatus = localStorage.getItem("status");
+        if (getStatus && getStatus === "close") {
+            sidebar.classList.toggle("close");
+        }
+
+        modeToggle.addEventListener("click", () => {
+            body.classList.toggle("dark");
+            if (body.classList.contains("dark")) {
+                localStorage.setItem("mode", "dark");
+            } else {
+                localStorage.setItem("mode", "light");
+            }
+        });
+
+        sidebarToggle.addEventListener("click", () => {
+            sidebar.classList.toggle("close");
+            if (sidebar.classList.contains("close")) {
+                localStorage.setItem("status", "close");
+            } else {
+                localStorage.setItem("status", "open");
+            }
+        })
+    </script>
+</body>
+
+</html>
---
+++ Form/login.php
@@ -0,0 +1,153 @@
+<html>
+
+<head>
+    <title>
+        MNC Login
+    </title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&amp;display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/regis.css">
+    <link rel="stylesheet" href="../css/animationLoad.css">
+
+</head>
+
+<body>
+    <!-- Overlay animasi loading -->
+    <div class="loading-overlay" id="loadingOverlay">
+        <div class="atom-spinner">
+            <div class="spinner-inner">
+                <div class="spinner-circle"></div>
+                <div class="spinner-circle"></div>
+                <div class="spinner-circle"></div>
+            </div>
+        </div>
+    </div>
+
+    <div class="left">
+        <div class="logo">
+            <img height="80vh" width="100%" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/MNCTV_logo.png/798px-MNCTV_logo.png?20180109104219" alt="logo-mnc">
+        </div>
+        <h1>
+            Hallo
+            <span>
+                Pengiklan
+            </span>
+            👋
+        </h1>
+        <p>
+            Pasang Iklan Lewat MNC Akan Menambah Jumlah Pengunjung Anda :)
+        </p>
+        <div class="footer">
+            © 2024 YOGA MNC
+        </div>
+    </div>
+    <div class="right">
+        <div class="container">
+            <h2>
+                MNC Mania
+            </h2>
+            <p>
+                Selamat Datang
+            </p>
+            <p>
+                Sudah Punya Akun?
+                <a href="pendaftaran.php" data-bs-toggle="modal" data-bs-target="#registrationModal">
+                    Silahkan Buat Akun
+                </a>
+            </p>
+            <form method="POST" onsubmit="showLoading(); return false">
+                <input placeholder="yoga@gmail.com" type="email" name="email" />
+                <input placeholder="Password" type="password" name="password" />
+                <a href="#">
+                    <button>
+                        Login Sekarang
+                    </button>
+                </a>
+            </form>
+            <a href="#" class="google-login" style="text-decoration: none; color:black;">
+                <img alt="Google logo" height="40" src="https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png" />
+                Login dengan Google
+            </a>
+            <div class="forgot-password">
+                <a href="#">
+                    Klik Disini
+                </a>
+                untuk reset password
+            </div>
+        </div>
+    </div>
+    <!-- Formulir Registrasi Mengambang -->
+    <div class="modal fade" id="registrationModal" tabindex="-1" aria-labelledby="registrationModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="registrationModalLabel">Form Registrasi</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="nama-lengkap">Nama Lengkap</label>
+                        <input type="text" id="nama-lengkap" placeholder="Masukan Nama Lengkap">
+                    </div>
+                    <div class="form-group">
+                        <label for="username">Alamat</label>
+                        <input type="text" id="alamat" placeholder="Masukan Alamat">
+                    </div>
+                    <div class="form-group">
+                        <label for="kode-pos">Kode Pos</label>
+                        <input type="number" id="kode-pos" placeholder="Masukan Kode Pos">
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" id="email" placeholder="Masukan Email">
+                    </div>
+                    <div class="form-group">
+                        <label for="nomor-telepon">Nomor Telepon</label>
+                        <input type="number" id="nomor-telepon" placeholder="08953332254">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" id="password" placeholder="Masukan password">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="ulangi-password">Ulangi Password</label>
+                        <input type="password" id="ulangi-password" placeholder="Masukan ulang password">
+                    </div>
+                    <div class="form-group">
+                        <label>Jenis Kelamin</label>
+                        <div class="radio-group">
+                            <input type="radio" id="pria" name="jenis-kelamin" value="pria">
+                            <label for="pria">Pria</label>
+                            <input type="radio" id="wanita" name="jenis-kelamin" value="wanita">
+                            <label for="wanita">Wanita</label>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <button class="submit-btn">Daftar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    </div>
+    <!--  -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/js/bootstrap.bundle.min.js"></script>
+    <script>
+        function showLoading() {
+            document.getElementById("loadingOverlay").classList.add("active");
+            // Simulasi proses login dengan penundaan
+            setTimeout(() => {
+                document.getElementById("loadingOverlay").classList.remove("active");
+                alert("Login berhasil!"); // Ganti dengan logika setelah login sukses
+                window.location.assign("../Admin/dashboard.php");
+            }, 2000);
+            
+        }
+    </script>
+</body>
+
+</html>
